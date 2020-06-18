0f2cc7ff9d66fe63c0d6fea09561ddfcb7fd7180
Eric Maccoux <43770992+ECMaccoux@users.noreply.github.com>
2020-06-18 17:51:47 -0500

    Fade-in/blur
    Added fade-in/blur effect to background image and typewriter text

--- JS/home.js
+++ JS/home.js
@@ -1,3 +1,19 @@
-function fadeIn(obj) {
+/*function fadeInBG(obj) {
     $(obj).fadeIn(1000);
+    setTimeout(blurBG(obj), 1000);
 }
+
+function blurBG(obj) {
+    $(obj).addClass('blur');
+}
+
+function onLoadBG(obj) {
+    fadeInBG(obj);
+}*/
+
+$(function() {
+    $('#bg-img').fadeIn(1000).delay(1000);
+    setTimeout(function() {
+        $('#bg-img')
+    }, 1000);
+});
